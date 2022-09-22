115baf29a7cd93b071ee41255c1fc431c101b934
GwangrokBaek <zester926@gmail.com>
2022-09-22 16:42:45 +0900

    prod 용 환경 구성

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -35,19 +35,20 @@
 #undef  MQTT_HOST
 #define MQTT_HOST         "a3b608yz74ta5k-ats.iot.ap-northeast-2.amazonaws.com" // [MqttHost]
 #define MQTT_HOST_DEV    "a3b608yz74ta5k-ats.iot.ap-northeast-2.amazonaws.com" // [MqttHost]
-#define MQTT_HOST_PROD    "a3b608yz74ta5k-ats.iot.ap-northeast-2.amazonaws.com" // [MqttHost]
+#define MQTT_HOST_PROD    "a1xm0qwzswyej2-ats.iot.ap-northeast-2.amazonaws.com" // [MqttHost]
 
 #undef  MQTT_PORT
 #define MQTT_PORT         8883                   // [MqttPort] MQTT port (10123 on CloudMQTT)
 
 #ifdef FIRMWARE_ZIOT
 #define API_HOST_DEV          "api-ziot-tasmota.zigbang.net"
-#define API_HOST_PROD          "api-ziot-tasmota.zigbang.net"
+#define API_HOST_PROD          "api-ziot-tasmota.zigbang.com"
 #define LAMBDA_CERT_URL_DEV   "/certificate"
 #define LAMBDA_CERT_URL_PROD   "/certificate"
 
 // #define AWS_FINGERPRINT   "98 0a 41 04 6e 50 88 9c 0c d0 7f 73 21 12 e3 95 aa 67 7f af"
-#define AWS_FINGERPRINT   "86 8f ab fc d2 8c 4f 66 03 37 48 98 24 b7 32 96 4c 4e 6b 33"
+#define AWS_FINGERPRINT_DEV   "86 8f ab fc d2 8c 4f 66 03 37 48 98 24 b7 32 96 4c 4e 6b 33"
+#define AWS_FINGERPRINT_PROD   "86 8f ab fc d2 8c 4f 66 03 37 48 98 24 b7 32 96 4c 4e 6b 33"
 #else
 #define API_HOST_DEV          "3srkj4h3d9.execute-api.ap-northeast-2.amazonaws.com"
 #define API_HOST_PROD          "3srkj4h3d9.execute-api.ap-northeast-2.amazonaws.com"
@@ -243,89 +244,6 @@
   //   #undef IR_RCV_WHILE_SENDING                             // Turns on receiver while sending messages, i.e. receive your own. This is unreliable and can cause IR timing issues   
 
   #undef USE_BERRY
-
-// -- HTTP GUI Colors -----------------------------
-// HTML hex color codes. Only 3 and 6 digit hex string values are supported!! See https://www.w3schools.com/colors/colors_hex.asp
-// Light theme - pre v7
-// WebColor {"WebColor":["#000","#fff","#f2f2f2","#000","#fff","#000","#fff","#f00","#008000","#fff","#1fa3ec","#0e70a4","#d43535","#931f1f","#47c266","#5aaf6f","#fff","#999","#000"]}
-/*
-#define COLOR_TEXT                  "#000"       // [WebColor1] Global text color - Black
-#define COLOR_BACKGROUND            "#fff"       // [WebColor2] Global background color - White
-#define COLOR_FORM                  "#f2f2f2"    // [WebColor3] Form background color - Greyish
-#define COLOR_INPUT_TEXT            "#000"       // [WebColor4] Input text color - Black
-#define COLOR_INPUT                 "#fff"       // [WebColor5] Input background color - White
-#define COLOR_CONSOLE_TEXT          "#000"       // [WebColor6] Console text color - Black
-#define COLOR_CONSOLE               "#fff"       // [WebColor7] Console background color - White
-#define COLOR_TEXT_WARNING          "#f00"       // [WebColor8] Warning text color - Red
-#define COLOR_TEXT_SUCCESS          "#008000"    // [WebColor9] Success text color - Dark lime green
-#define COLOR_BUTTON_TEXT           "#fff"       // [WebColor10] Button text color - White
-#define COLOR_BUTTON                "#1fa3ec"    // [WebColor11] Button color - Vivid blue
-#define COLOR_BUTTON_HOVER          "#0e70a4"    // [WebColor12] Button color when hovered over - Dark blue
-#define COLOR_BUTTON_RESET          "#d43535"    // [WebColor13] Restart/Reset/Delete button color - Strong red
-#define COLOR_BUTTON_RESET_HOVER    "#931f1f"    // [WebColor14] Restart/Reset/Delete button color when hovered over - Dark red
-#define COLOR_BUTTON_SAVE           "#47c266"    // [WebColor15] Save button color - Moderate lime green
-#define COLOR_BUTTON_SAVE_HOVER     "#5aaf6f"    // [WebColor16] Save button color when hovered over - Dark moderate lime green
-#define COLOR_TIMER_TAB_TEXT        "#fff"       // [WebColor17] Config timer tab text color - White
-#define COLOR_TIMER_TAB_BACKGROUND  "#999"       // [WebColor18] Config timer tab background color - Dark gray
-#define COLOR_TITLE_TEXT            "#000"       // [WebColor19] Title text color - Whiteish
-*/
-// Dark theme
-// WebColor {"WebColor":["#eaeaea","#252525","#4f4f4f","#000","#ddd","#65c115","#1f1f1f","#ff5661","#008000","#faffff","#1fa3ec","#0e70a4","#d43535","#931f1f","#47c266","#5aaf6f","#faffff","#999","#eaeaea"]}
-#define COLOR_TEXT                  "#e9e9e9"    // [WebColor1] Global text color - Very light gray
-#define COLOR_BACKGROUND            "#31353c"    // [WebColor2] Global background color - Very dark gray (mostly black)
-#define COLOR_FORM                  "#31353c"    // [WebColor3] Form background color - Very dark gray
-#define COLOR_INPUT_TEXT            "#000"       // [WebColor4] Input text color - Black
-#define COLOR_INPUT                 "#ddd"       // [WebColor5] Input background color - Very light gray
-#define COLOR_CONSOLE_TEXT          "#65c115"    // [WebColor6] Console text color - Strong Green
-#define COLOR_CONSOLE               "#1f1f1f"    // [WebColor7] Console background color - Very dark gray (mostly black)
-#define COLOR_TEXT_WARNING          "#da4139"    // [WebColor8] Warning text color - Brick Red
-#define COLOR_TEXT_SUCCESS          "#008000"    // [WebColor9] Success text color - Dark lime green
-#define COLOR_BUTTON_TEXT           "#fff"    // [WebColor10] Button text color - Very pale (mostly white) cyan
-#define COLOR_BUTTON                "#ffa400"    // [WebColor11] Button color - Vivid blue
-#define COLOR_BUTTON_HOVER          "#996300"    // [WebColor12] Button color when hovered over - Dark blue
-#define COLOR_BUTTON_RESET          "#da4139"    // [WebColor13] Restart/Reset/Delete button color - Strong red
-#define COLOR_BUTTON_RESET_HOVER    "#931f1f"    // [WebColor14] Restart/Reset/Delete button color when hovered over - Dark red
-#define COLOR_BUTTON_SAVE           "47c266"    // [WebColor15] Save button color - Moderate lime green
-#define COLOR_BUTTON_SAVE_HOVER     "#5aaf6f"    // [WebColor16] Save button color when hovered over - Dark moderate lime green
-#define COLOR_TIMER_TAB_TEXT        "#faffff"    // [WebColor17] Config timer tab text color - Very pale (mostly white) cyan.
-#define COLOR_TIMER_TAB_BACKGROUND  "#999"       // [WebColor18] Config timer tab background color - Dark gray
-#define COLOR_TITLE_TEXT            "#e9e9e9"    // [WebColor19] Title text color - Very light gray
-
-/*
-Examples :
-// -- Master parameter control --------------------
-#undef  CFG_HOLDER
-#define CFG_HOLDER        4617                   // [Reset 1] Change this value to load SECTION1 configuration parameters to flash
-// -- Setup your own Wifi settings  ---------------
-#undef  STA_SSID1
-#define STA_SSID1         "YourSSID"             // [Ssid1] Wifi SSID
-#undef  STA_PASS1
-#define STA_PASS1         "YourWifiPassword"     // [Password1] Wifi password
-// -- Setup your own MQTT settings  ---------------
-#undef  MQTT_HOST
-#define MQTT_HOST         "your-mqtt-server.com" // [MqttHost]
-#undef  MQTT_PORT
-#define MQTT_PORT         1883                   // [MqttPort] MQTT port (10123 on CloudMQTT)
-#undef  MQTT_USER
-#define MQTT_USER         "YourMqttUser"         // [MqttUser] Optional user
-#undef  MQTT_PASS
-#define MQTT_PASS         "YourMqttPass"         // [MqttPassword] Optional password
-// You might even pass some parameters from the command line ----------------------------
-// Ie:  export PLATFORMIO_BUILD_FLAGS='-DUSE_CONFIG_OVERRIDE -DMY_IP="192.168.1.99" -DMY_GW="192.168.1.1" -DMY_DNS="192.168.1.1"'
-#ifdef MY_IP
-#undef  WIFI_IP_ADDRESS
-#define WIFI_IP_ADDRESS     MY_IP                // Set to 0.0.0.0 for using DHCP or enter a static IP address
-#endif
-#ifdef MY_GW
-#undef  WIFI_GATEWAY
-#define WIFI_GATEWAY        MY_GW                // if not using DHCP set Gateway IP address
-#endif
-#ifdef MY_DNS
-#undef  WIFI_DNS
-#define WIFI_DNS            MY_DNS               // If not using DHCP set DNS IP address (might be equal to WIFI_GATEWAY)
-#endif
-// !!! Remember that your changes GOES AT THE BOTTOM OF THIS FILE right before the last #endif !!! 
-*/
 
 #undef DEBUG_TASMOTA_CORE                       // Enable core debug messages
 #undef DEBUG_TASMOTA_DRIVER                     // Enable driver debug messages
